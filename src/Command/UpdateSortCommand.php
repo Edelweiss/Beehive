53189de561ddc50b2026b39674d1c7f7d562a1bf
Elemmire <elemmire@ossiriand.de>
2025-01-08 15:35:15 +0100

    editie sort

--- src/Command/UpdateSortCommand.php
+++ src/Command/UpdateSortCommand.php
@@ -38,7 +38,7 @@ class UpdateSortCommand extends Command
               echo '' . $editionId . "\n";
               
               $repository = $this->entityManager->getRepository(Correction::class);
-              $corrections = $repository->findBy(['edition', $editionId . '']);
+              $corrections = $repository->findBy(['edition', '1']);
               var_dump($corrections);
           }
           
